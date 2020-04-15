8e5038e0936025bb9b89edede82da4c7baa8fa08
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-15 14:41:32 +0900

    [Update] homeコントローラへのルーティングをresouces only化

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'home/about'
+  resources :home, only: [:index, :about]
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 

015064f36c231ea73c0817bfacde839e7c1cb627
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-15 15:17:58 +0900

    [Update] home_aboutへのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :home, only: [:index, :about]
-
+  root 'home#index'
+  get 'home/about', to: 'home#about', as: 'home_about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # root 'books#index'
 end

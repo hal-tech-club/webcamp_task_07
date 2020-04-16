d45286323435cd94d999291073691c9d5e98d411
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-16 14:50:16 +0900

    [Fix] usersへのルーティングをresources化, home_aboutの名前設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  root 'home#index'
-  get 'home/about', to: 'home#about', as: 'home_about'
+  get 'home/about', as: 'home_about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  resources :users
 end

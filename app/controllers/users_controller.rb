02c354951a4b34df590fdf1d105ac380ecf7b8fa
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-16 15:12:51 +0900

    [Add] ログインユーザーを全ページ初回アクセス時@userで受け取るようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   # users controller下は認証ガード保護有
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :find_login_user
 
   def index
     @users = User.all
@@ -12,5 +12,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  protected
+
+  def find_login_user
+    @user = User.find(current_user.id)
   end
 end

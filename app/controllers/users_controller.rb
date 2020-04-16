4ca5e43bdcb0ab822b42033a15010c555056d1f7
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-16 12:45:33 +0900

    [Add] usersコントローラの各アクションにUserモデル取得機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(paramas[:id])
   end
 
   def edit
+    @user = User.find(paramas[:id])
   end
 end

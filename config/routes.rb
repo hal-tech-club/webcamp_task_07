55a2b2b0681768ac9e0880712dd8d09be87d7893
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-16 11:25:05 +0900

    [Add] Usersコントローラと各viewsを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   root 'home#index'
   get 'home/about', to: 'home#about', as: 'home_about'
   devise_for :users

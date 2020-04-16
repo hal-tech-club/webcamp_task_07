e4c6354058b0f0e0f7ab5dbc116781728439c5f9
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-16 15:10:45 +0900

    [Update] ロールバック&マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_075156) do
+ActiveRecord::Schema.define(version: 2020_04_16_060937) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_075156) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "profile_image_id"
-    t.text "inrtoduction"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

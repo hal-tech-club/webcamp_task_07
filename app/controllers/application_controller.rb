2b02f1eb257834e88e46f184941ca28e5ab0eb06
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-16 12:54:52 +0900

    [Fix] configure_permitted_parametersを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, :authenticate_user! # ログインガード
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
-  def configure_permitted_parameters
-    added_attrs = [:email, :name, :password, :password_confirmation]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
-  end
 
   def after_sign_in_path_for(resource)
     user_path(current_user.id)
@@ -17,5 +9,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     root
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys: [:email]
   end
 end

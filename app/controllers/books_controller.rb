d650d08784d3bbba5b5c8642cd2029608845cc3b
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-17 22:49:49 +0900

    [Update] Booksコントローラの各アクションにバリデーションチェック 機能を実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,10 +6,16 @@ class BooksController < ApplicationController
 
   # Book登録
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
-    redirect_to book_path(book.id)
+    @new_book = Book.new(book_params)
+    @new_book.user_id = current_user.id
+    if @new_book.save
+      flash[:success] = 'Book was successfully created'
+      redirect_to book_path(@new_book.id)
+    else
+      @books = Book.all
+      @user = User.find(current_user.id)
+      render action: :index
+    end
   end
 
   # Book一覧画面表示
@@ -31,15 +37,20 @@ class BooksController < ApplicationController
 
   # Book更新
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:success] = 'Book was successfully updated'
+      redirect_to book_path(@book.id)
+    else
+      render action: :edit
+    end
   end
 
   # Book削除
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:success] = 'Book was successfully destroyed.'
     redirect_to books_path
   end
 

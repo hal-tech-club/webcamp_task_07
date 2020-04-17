8de46b59384372df767f85038d96b5ee91d80673
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-17 23:18:43 +0900

    [Update] Rspec用マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_04_17_070345) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "profile_image_id"
+    t.text "inrtoduction"
     t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

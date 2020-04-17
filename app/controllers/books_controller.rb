f977784fe4d4e14775a7a304b9f1dac6786e7168
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-17 15:27:35 +0900

    [Add] booksコントローラの必要となる各アクションの宣言

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,30 @@
 class BooksController < ApplicationController
   # books controller下は認証ガード保護有
   before_action :authenticate_user!
+
+  # newに当たる部分テンプレートはほぼ常時表示
+
+  # Book登録
+  def create
+  end
+
+  # Book一覧画面表示
+  def index
+  end
+
+  # Book詳細画面表示
+  def show
+  end
+
+  # Book編集画面表示
+  def edit
+  end
+
+  # Book更新
+  def update
+  end
+
+  # Book削除
+  def destroy
+  end
 end

3248f91040eb0412dec9fd9e8fad8aaf33a0b41e
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-05-12 15:30:39 +0900

    fix: favorited_by?のwhere句の綴りを修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,6 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   def favorited_by?(user)
-    favorites.wehre(user_id: user.id).exists?
+    favorites.where(user_id: user.id).exists?
   end
 end

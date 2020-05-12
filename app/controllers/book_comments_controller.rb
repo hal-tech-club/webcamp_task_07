03f1806bca0fe4153fff6f8f210229aa4e5bdaef
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-05-12 18:27:34 +0900

    add: book_comments#destroyがコメント投稿者本人以外実行できないようにした

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,4 +1,6 @@
 class BookCommentsController < ApplicationController
+  before_action :correct_user, only: [:destroy]
+
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
@@ -12,6 +14,15 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.find_by(book_id: book.id)
     comment.destroy
     redirect_to book_path(book)
+  end
+
+  protected
+
+  def correct_user
+    user = BookComment.find_by(book_id: params[:book_id]).user
+    if current_user.id != user.id
+      redirect_to books_path
+    end
   end
 
   private

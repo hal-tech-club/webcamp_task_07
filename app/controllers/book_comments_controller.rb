ce66349d049d41f8ef0cf5eea9b63dc4196ba1ea
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-05-12 17:04:39 +0900

    add: StrongParameters実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,4 +2,10 @@ class BookCommentsController < ApplicationController
   def create
 
   end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:user_id, :book_id, :comment)
+  end
 end
